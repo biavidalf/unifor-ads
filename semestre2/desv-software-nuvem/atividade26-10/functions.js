2ea8740854311ee267cf620cb54b2ad4e0e30028
LABORATORIO T18 <alunot18@acad.unifor.br>
2022-10-26 19:40:48 -0300

    Adicionando arquivo com as funcoes

---
+++ semestre2/desv-software-nuvem/atividade26-10/functions.js
@@ -0,0 +1,23 @@
+function soma(a, b){
+    return a+b;
+}
+
+function sub(a, b){
+    return a-b;
+}
+
+function mut(a, b){
+    return a*b;
+}
+
+function div(a, b){
+    return a/b;
+}
+
+function piso(){
+    return false;
+}
+
+function teto(){
+    return false;
+}
