b83c90ae8842d6d91620ab068cb352d5e80d7c45
Beatriz Vidal <biavidalf@gmail.com>
2022-10-28 15:31:27 -0300

    Renomeando variaveis e adicionando loop perguntar

--- semestre2/ambientes/entregavel4/main.js
+++ semestre2/ambientes/entregavel4/main.js
@@ -20,59 +20,71 @@ console.log(`===============================
 * ================= ALGORITMO 1 =================
 */
 console.log("ALGORITMO 1: Número Primo");
-let numero = parseFloat(readlineSync.question('Digite um numero: '));
-console.log(numeroPrimo(numero) + "\n");
+let input_numeroPrimo = perguntar('Digite um numero: ');
+console.log(numeroPrimo(input_numeroPrimo) + "\n");
 
 /*
 * ================= ALGORITMO 2 =================
 */
 console.log("ALGORITMO 2: Somar números");
-let quantidade = readlineSync.question('Quantos numeros deseja somar? ');
-let numeros = [];
+let input_quantidadeNumerosSoma = readlineSync.question('Quantos numeros deseja somar? ');
+let numerosParaSomar = [];
 
-for(let i = 0; i < quantidade; i++){
+for(let i = 0; i < input_quantidadeNumerosSoma; i++){
   let numAtual = parseFloat(readlineSync.question('Digite o valor ' + (i+1) + ': '));
-  numeros[i] = numAtual;
+  numerosParaSomar[i] = numAtual;
 }
-console.log(somarNumeros(numeros) + "\n");
+console.log(somarNumeros(numerosParaSomar) + "\n");
 
 /*
 * ================= ALGORITMO 3 =================
 */
 console.log("ALGORITMO 3: Série Fibonacci");
-let termos = parseInt(readlineSync.question('Digite a quantidade de termos: '));
-console.log(fibonacci(termos) + "\n");
+let input_numTermos = parseInt(readlineSync.question('Digite a quantidade de termos: '));
+console.log(fibonacci(input_numTermos) + "\n");
 
 /*
 * ================= ALGORITMO 4 =================
 */
 console.log("ALGORITMO 4: MDC");
-let numero1 = parseFloat(readlineSync.question('Digite o primeiro valor: '));
-let numero2 = parseFloat(readlineSync.question('Digite o segundo valor: '));
-console.log(mdc(numero1, numero2) + "\n");
+let input_mdcNum1 = parseFloat(readlineSync.question('Digite o primeiro valor: '));
+let input_mdcNum2 = parseFloat(readlineSync.question('Digite o segundo valor: '));
+console.log(mdc(input_mdcNum1, input_mdcNum2) + "\n");
 
 /*
   * ================= ALGORITMO 5 =================
 */
 console.log("ALGORITMO 5: Ordenar Vetor");
-quantidade = readlineSync.question('Quantos numeros deseja ordenar? ');
-numeros = [];
+let input_quantidadeNumerosOrdenar = readlineSync.question('Quantos numeros deseja ordenar? ');
+let numerosParaOrdenar = [];
 
-for(let i = 0; i < quantidade; i++){
+for(let i = 0; i < input_quantidadeNumerosOrdenar; i++){
   let numAtual = parseFloat(readlineSync.question('Digite o valor ' + (i+1) + ': '));
-  numeros[i] = numAtual;
+  numerosParaOrdenar[i] = numAtual;
 }
-console.log(ordenarVetor(numeros) + "\n");
+console.log(ordenarVetor(numerosParaOrdenar) + "\n");
 
 /*
   * ================= ALGORITMO 6 =================
 */
 console.log("ALGORITMO 6: Contar Inteiros");
-quantidade = readlineSync.question('Quantos numeros deseja contar? ');
-numeros = [];
+let input_quantidadeNumerosContar = readlineSync.question('Quantos numeros deseja contar? ');
+let numerosParaContar = [];
 
-for(let i = 0; i < quantidade; i++){
+for(let i = 0; i < input_quantidadeNumerosContar; i++){
   let numAtual = parseFloat(readlineSync.question('Digite o valor ' + (i+1) + ': '));
-  numeros[i] = numAtual;
+  numerosParaContar[i] = numAtual;
 }
-console.log(contagem(numeros));
+console.log(contagem(numerosParaContar));
+
+
+// Função para realizar a entrada de dados
+// Faz um loop até o usuário digitar um número válido
+// Caso o usuário digitar nada ou apenas letras, ele pergunta novamente
+function perguntar(mensagem, tipo = 'float'){
+  let resposta = (tipo == 'int') ? parseInt(readlineSync.question(mensagem)) : parseFloat(readlineSync.question(mensagem));
+  while(isNaN(resposta)){
+    resposta = perguntar(mensagem, tipo);
+  }
+  return resposta;
+}
